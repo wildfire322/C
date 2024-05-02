9f32252a47fae251d8858dbfb7a7dbcf59c1862e
pxx <pxl322@outlook.com>
2024-05-02 11:53:08 +0800

    Add generator.c file for generating test data

--- project.c
+++ project.c
@@ -12,7 +12,7 @@ int test[1001];
 int visited[1001];
 
 // Structure to represent a graph
-typedef struct {
+typedef struct Graph {
     int nv; // Number of vertices
     int ne; // Number of edges
     int e[1001][1001]; // Adjacency matrix to store edge weights
@@ -20,7 +20,7 @@ typedef struct {
 
 // Function to create a graph
 graph create(int nv, int ne) {
-    graph g = (graph)malloc(sizeof(graph));
+    graph g = (graph)malloc(sizeof(struct Graph));
     g->nv = nv;
     g->ne = ne;
 
@@ -90,12 +90,75 @@ int dijkstra(graph g) {
     }
     return 1;
 }
+int dijkstra_file(graph g, FILE* file){
+    int kk, z;
 
+    // Read the test case values and initialize distance and visited arrays
+    for (int i = 0; i < g->nv; i++) {
+        fscanf(file, "%d", &kk); // Read from file instead of standard input
+        distance[i] = inf;
+        visited[i] = 0;
+        test[i] = kk - 1;
+    }
+
+
+    // Set the distance of the source vertex to 0 and mark it as visited
+    distance[test[0]] = 0;
+    visited[test[0]] = 1;
+
+    // Update the distance array with the weights of the edges connected to the source vertex
+    for (int i = 0; i < g->nv; i++) {
+        if (i != test[0] && g->e[test[0]][i] != inf) {
+            distance[i] = g->e[test[0]][i];
+        }
+    }
+
+    // Perform Dijkstra's algorithm for the remaining vertices
+    for (int i = 1; i < g->nv; i++) {
+        z = findmin(g);
+
+        // If the distance of the current vertex is not equal to the distance of the previous vertex, return 0
+        if (distance[z] != distance[test[i]]) {
+            return 0;
+        }
+
+        // Mark the current vertex as visited
+        visited[test[i]] = 1;
+
+        // Update the distance array with the weights of the edges connected to the current vertex
+        for (int j = 0; j < g->nv; j++) {
+            if (!visited[j] && g->e[test[i]][j] != inf && distance[test[i]] + g->e[test[i]][j] < distance[j]) {
+                distance[j] = distance[test[i]] + g->e[test[i]][j];
+            }
+        }
+    }
+    return 1;
+}
 int main() {
     int m, n;
+    FILE* file;
+
+    // Ask the user for the input method
+    printf("Enter 1 for manual input, 2 for file input: ");
+    int method;
+    scanf("%d", &method);
+    getchar(); // This is to capture the newline character after entering the method
+
+    if (method == 2) {
+        // If the user selects file input, open the file
+        file = fopen("test_data.txt", "r");
+        if (file == NULL) {
+            fprintf(stderr, "Failed to open the file\n");
+            return EXIT_FAILURE;
+        }
+    }
 
     // Read the number of vertices and edges
-    scanf("%d %d", &n, &m);
+    if (method == 1) {
+        scanf("%d %d", &n, &m);
+    } else {
+        fscanf(file, "%d %d", &n, &m);
+    }
 
     // Create a graph
     graph g = create(n, m);
@@ -103,25 +166,39 @@ int main() {
     // Read the edges and their weights
     for (int i = 0; i < m; i++) {
         int a, b, c;
-        scanf("%d %d %d", &a, &b, &c);
+        if (method == 1) {
+            scanf("%d %d %d", &a, &b, &c);
+        } else {
+            fscanf(file, "%d %d %d", &a, &b, &c);
+        }
         g->e[a - 1][b - 1] = c;
         g->e[b - 1][a - 1] = c;
     }
 
-    int k, u;
-
     // Read the number of test cases
-    scanf("%d", &k);
+    int k,u;
+    if (method == 1) {
+        scanf("%d", &k);
+    } else {
+        fscanf(file, "%d", &k);
+    }
 
     // Perform Dijkstra's algorithm for each test case
     for (int i = 0; i < k; i++) {
-        u = dijkstra(g);
+        if (method == 1) {
+            u = dijkstra(g);
+        } else {
+            u = dijkstra_file(g, file);
+        }
         if (u == 1) {
             printf("Yes\n");
         }// If the function returns 1,which means it does be a probable Dijkstra Sequence,so print "Yes"
         else {
             printf("No\n");
         }// If the function returns 0,which means it does not be a probable Dijkstra Sequence,so print "No"
+    }
+    if (method == 2) {
+        fclose(file);
     }
     return 0;
 }
